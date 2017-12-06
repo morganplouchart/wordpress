32dcc141000669c884d5cfbf92bbb4f03c48dd41
morganplouchart <morgan.plouchart@yahoo.fr>
2017-12-06 11:29:37 +0100

    c'est polo qui le dit

--- wordpress.sh
+++ wordpress.sh
@@ -9,16 +9,18 @@ show_menu(){
     ENTER_LINE=`echo "\033[33m"`
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${MENU}**${NUMBER} 0)${MENU} Restart Apache2 ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 1)${MENU} PHP 7.0 ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 2)${MENU} Apache2 ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 3)${MENU} PHP 7.0 MY SQL ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 4)${MENU} libapache2-mod-php7.0 ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 5)${MENU} Création Base De Données${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 6)${MENU} Installation de Wordpress étape 1 (Cli)${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 7)${MENU} Installation de Wordpress étape 2 (Droit fichier)${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 8)${MENU} Installation de Wordpress étape 3 (Téléchargement du core)${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 9)${MENU} Installation de Wordpress étape 4 (Création de la base de données de Wordpress)${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 10)${MENU} Installation de Wordpress étape 5 (Vous êtes arrivée a la fin !)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Installation de la configuration minimum ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Création Base De Données${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Installation de Wordpress étape 1 (Cli)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 4)${MENU} Installation de Wordpress étape 2 (Droit fichier)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU} Installation de Wordpress étape 3 (Téléchargement du core)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 6)${MENU} Installation de Wordpress étape 4 (Création de la base de données de Wordpress)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 7)${MENU} Installation de Wordpress étape 5 (Vous êtes arrivée a la fin !)${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 8)${MENU} Search un plugin ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 9)${MENU} Installer un plugin ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 10)${MENU} Activer un plugin ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 11)${MENU} Désactiver un plugin ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 12)${MENU} Supprimer un plugin ${NORMAL}"
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
     read opt
@@ -42,56 +44,42 @@ while [ opt != '' ]
         menu;
             ;;
         1) clear;
-        option_picked "PHP 7 est en cours d'installation ...";
-        sudo apt-get install php7.0; option_picked "PHP 7 est installer"; show_menu;
+        option_picked "Installer la configuration minimum.";
+        sudo apt-get install php7.0;
+        sudo apt-get install apache2;
+        sudo apt-get install php7.0-mysql;
+        sudo apt-get install libapache2-mod-php7.0;
+        option_picked "PHP 7 est installer"; show_menu;
         menu;
 
         ;;
 
         2) clear;
-            option_picked "Apache 2 est en cours d'installation ...";
-            sudo apt-get install apache2;  option_picked "Apache2 est installer"; show_menu;
-        menu;
-            ;;
-
-        3) clear;
-            option_picked "PHP 7 MY SQL est en cours d'installation ...";
-        sudo apt-get install php7.0-mysql; option_picked "PHP 7 MY SQL est installer"; show_menu;
-        menu;
-            ;;
-
-        4) clear;
-            option_picked "libapache2-mod-php7.0 est en cours d'installation ...";
-        sudo apt-get install libapache2-mod-php7.0; option_picked "libapache2-mod-php7.0 est installer"; show_menu;
-        menu;
-            ;;
-
-        5) clear;
             option_picked "La base de données est en cours de création ...";
         sudo mysql -uroot -p; option_picked "L'user est root et le mot de passe est celui que vous avez choisi"; show_menu;
         menu;
             ;;
 
-        6) clear;
+        3) clear;
             option_picked "Prémiere étape de l'installation de Wordpress";
         sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; option_picked "La prémiere étape est terminé"; show_menu;
         menu;
             ;;
 
 
-        7) clear;
+        4) clear;
             option_picked "Seconde étape de l'installation de Wordpress";
         sudo chmod 777 wp-cli.phar; sudo mv wp-cli.phar /usr/local/bin/wp; option_picked "La Seconde étape est terminé"; show_menu;
         menu;
             ;;
 
-        8) clear;
+        5) clear;
             option_picked "troisième étape de l'installation de Wordpress";
         sudo wp core download --allow-root; option_picked "La troisième étape est terminé, l'installation de WordPress est terminé"; show_menu;
         menu;
             ;;
 
-        9) clear;
+        6) clear;
             option_picked "Quatriéme étape de l'installation de Wordpress";
             echo "entrée le nom de votre base de donnée"
             read -e dbname break;
@@ -102,7 +90,7 @@ while [ opt != '' ]
         sudo wp config create --dbname=$dbname --dbuser=$user --dbpass=$password --allow-root; option_picked "La quatrième étape est terminé, l'installation de WordPress est terminé"; show_menu;
         menu;
             ;;
-            10) clear;
+            7) clear;
                 option_picked "derniere étape de l'installation de Wordpress";
                 echo "entrée votre URL"
                 read -e urlname break;
@@ -117,6 +105,46 @@ while [ opt != '' ]
             sudo wp core install --url=$urlname --title=$titre --admin_user=$admin --admin_password=$password --admin_email=$email --allow-root; option_picked "La derniere étape est terminé, l'installation de WordPress est terminé"; show_menu;
             menu;
                 ;;
+
+           8) clear;
+            option_picked "Search un plugin";
+            echo "entrée le nom d'un plugin à rechercher"
+            read -e cherche break;
+            sudo wp plugin search $cherche --allow-root; option_picked "Recherche terminer"; show_menu;
+            menu;
+            ;;
+
+           9) option_picked "Installer un plugin";
+              echo "entrée le nom d'un plugin à installer"
+              read -e install break;
+              sudo wp plugin install $install --allow-root; option_picked "Recherche terminer"; show_menu;
+              menu;
+              ;;
+            
+           10) 
+              option_picked "activer un plugin";
+              echo "entrée le nom d'un plugin à activer"
+              read -e active break;
+              sudo wp plugin activate $active --allow-root; option_picked "Recherche terminer"; show_menu;
+              menu;
+              ;;
+
+            11)
+              option_picked "désactiver un plugin";
+              echo "entrée le nom d'un plugin à désactiver"
+              read -e desactive break;
+              sudo wp plugin deactivate $desactive --allow-root; option_picked "Recherche terminer"; show_menu;
+              menu;
+              ;;
+
+             12)
+              option_picked "désactiver un plugin";
+              echo "entrée le nom d'un plugin à supprimer"
+              read -e uninstall break;
+              sudo wp plugin uninstall $uninstall --allow-root; option_picked "Recherche terminer"; show_menu;
+              menu;
+              ;;
+        
         x)exit;
         ;;
 
